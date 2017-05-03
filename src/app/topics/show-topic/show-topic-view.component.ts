976edaa7a7f769189a82ad104315e8e6f179dc5b
NicolaiMensel <nicolai.m.jorgensen@gmail.com>
2017-05-03 16:03:38 +0200

    Show-Topic view
    Working on making comments inside comments

--- src/app/topics/show-topic/show-topic-view.component.ts
+++ src/app/topics/show-topic/show-topic-view.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Topic} from "../topic";
+import {Content} from "../content";
 
 @Component({
   selector: 'app-show-topic-view',
@@ -11,17 +12,41 @@ export class ShowTopicViewComponent implements OnInit {
   @Input()
   topic : Topic;
 
+  newComment : Topic;
+  subComment : Topic;
+
   @Output()
   tryAddCommentEmitter = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.initTopicTemplates()
   }
 
   getDateAsString(timeStamp : string){
     var date = new Date(timeStamp);
     return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+  }
+
+  addComment(comment : Topic, parentTopic : Topic)
+  {
+    comment.parent = parentTopic;
+    comment.title = "Re: " + comment.parent.title;
+    comment.timeStamp = new Date().toString();
+    //TO DOO
+    comment.user = parentTopic.user;
+    parentTopic.subTopics.push(comment);
+    this.initTopicTemplates();
+  }
+
+  initTopicTemplates(){
+    this.newComment = new Topic()
+    this.newComment.content = new Content();
+    this.subComment = new Topic();
+    this.subComment.content = new Content();
   }
 
 }
